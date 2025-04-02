d68a198016aae43a9d2fc33fa05e66d69b5aa780
Tiago Porto <tiago@tiagoporto.com>
2025-04-02 20:57:25 -0300

    chore: add prettier astro plugin

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,1 +1,16 @@
-export { default } from '@tiagoporto/prettier-config'
+import tiagoportoConfig from '@tiagoporto/prettier-config'
+
+/** @type {import("prettier").Config} */
+export default {
+  ...tiagoportoConfig,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    ...tiagoportoConfig.overrides,
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
