18f6ba6cc9a561c98f209e85dc376eaedee7a4f9
Tiago Porto <tiago@tiagoporto.com>
2025-04-02 09:12:44 -0300

    chore: set up lint staged

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,14 @@
+export default {
+  // '*': 'prettier --check --ignore-unknown --write',
+  "*.{md,markdown,mdx}": [
+    "remark --frail",
+    // 'eslint --max-warnings 0 --no-warn-ignored',
+  ],
+  // '*.{scss,css}': 'stylelint',
+  // '*.{ts,tsx}': () => 'tsc --project tsconfig.json',
+  // '*.{html,yml,json,jsonc,json5}': 'eslint --max-warnings 0 --no-warn-ignored',
+  // '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': [
+  //   'eslint --max-warnings 0 --no-warn-ignored',
+  //   'jest --bail --findRelatedTests --passWithNoTests',
+  // ],
+};
